7938ce4f6203a14ae23012be0bb0ecf0b9a55eb6
geekmatic <41346585+geekmatic@users.noreply.github.com>
2018-07-19 11:47:22 +0300

    Add files via upload

---
+++ Ypravlenie_dvigatelem/encoder.ino
@@ -0,0 +1,19 @@
+void func_encoder()
+{
+
+  if (millis() - timeOld >= 1000)
+  {
+    detachInterrupt(digitalPinToInterrupt(PIN_DO));
+    rpm = (pulses * 60) / 16;
+ 
+    timeOld = millis();
+    pulses = 0;
+    attachInterrupt(digitalPinToInterrupt(PIN_DO), counter, FALLING);
+    
+  }
+}
+
+void counter()
+{
+ pulses++;
+}
---
+++ Ypravlenie_dvigatelem/outs.ino
@@ -0,0 +1,11 @@
+void func_outs()
+{
+  int pwm;
+  if ((!digitalRead(koncevik1))&&(com>0)) com = 0;
+  if ((!digitalRead(koncevik3))&&(com<0)) com = 0;
+  digitalWrite(4, com!=0);
+  digitalWrite(5, com<0);
+  pwm = map(abs(com), 0, 255, 0, 255);
+  pwm = 255 - pwm;
+  analogWrite(3, pwm);
+}
